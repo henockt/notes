25c899294c40bc7c19c8c03a3a0d27d635a82339
henockt <henockt@users.noreply.github.com>
2020-11-17 16:54:35 +0300

    Added the ability to view notes by note id

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -1,9 +1,4 @@
-// redirections
-function redirect(id) {
-    document.getElementById(id).click();
-}
-
-// copied functionality of sleeping in milliseconds
+// sleeping in milliseconds
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -13,5 +8,18 @@ async function writeToId(str, id) {
     for (let i = 0; i < str.length ; i++) {
         await sleep(10);
         dom_elem.innerHTML += str[i];
+    }
+}
+
+// redirects to note showing page
+async function openNote() { 
+    if ($("#idInput").val().length != 10) {  
+        $('#invalidAlert').show();
+        await sleep(5000);
+        $('#invalidAlert').hide();
+    }
+    else {
+        $('#invalidAlert').hide();
+        window.location.assign("/shared/" + document.getElementById("idInput").value); 
     }
 }
